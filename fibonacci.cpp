dd5c5796ccabf11baf0349dd1f6a54132c1fe963
daoleno <daodaoonline@gmail.com>
2018-01-18 00:55:01 +0800

    update faster fibonacci

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -3,18 +3,33 @@
 using namespace std;
 
 // slow version
+// long fibonacci(int N) {
+//   if (N == 0)
+//     return 0;
+//   if (N == 1)
+//     return 1;
+//   return fibonacci(N - 1) + fibonacci(N - 2);
+// }
+
+// fast version
 long fibonacci(int N) {
   if (N == 0)
     return 0;
   if (N == 1)
     return 1;
-  return fibonacci(N - 1) + fibonacci(N - 2);
+  // save computed values
+  long calculated[N];
+  calculated[0] = 0;
+  calculated[1] = 1;
+  for (int i = 2; i <= N; ++i) {
+    calculated[i] = calculated[i - 1] + calculated[i - 2];
+  }
+  return calculated[N];
 }
 
 int main() {
-  int res;
   for (int N = 0; N < 100; N++) {
-    cout << N << " " << fibonacci(N) << endl;;
+    cout << N << " " << fibonacci(N) << endl;
   }
   return 0;
 }
