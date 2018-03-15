f361f688ddd101bf1f2ed1fc603e782d85575915
daoleno <daodaoonline@gmail.com>
2018-03-16 00:58:05 +0800

    add merge sort

--- sort.cpp
+++ sort.cpp
@@ -108,6 +108,73 @@ private:
   }
 };
 
+class Merge {
+public:
+  void sort(vector<string> &a) {
+    // aux.resize(a.size());
+    sort(a, 0, a.size() - 1);
+  }
+
+  void show(vector<string> a) {
+    for (auto i : a) {
+      cout << i << " ";
+    }
+    cout << endl;
+  }
+
+private:
+  // vector<string> aux;
+  bool less(string i, string j) { return i < j; }
+  void merge(vector<string> &a, int lo, int mid, int hi) {
+    // 将 a[lo..mid] 和 a[mid+1..hi] 归并
+    int i = lo, j = mid + 1;
+    vector<string> aux(a);
+    // aux.resize(a.size());
+    static int count = 0;
+    cout << "gua le" << count++ << endl;
+    // for (int k = lo; k <= hi; ++k) {
+    //   // cout << k << ": " << a[k] << endl;
+    //   aux[k] = a[k];
+    // }
+
+    // cout << "-----------" << endl;
+
+    // 从辅助数组 aux 向 结果数组 a 中放数据
+    for (int k = lo; k <= hi; ++k) {
+      if (j > mid) {
+        // 左半边用尽（取右边的元素）
+        cout << "----- begin ------" << endl;
+        cout << "k: " << k << "j: " << j << endl;
+        a[k] = aux[j++];
+        cout << "----- end -----" << endl;
+        cout << "************************************************************"
+             << endl;
+      } else if (j > hi) {
+        // 右半边用尽（取左边的元素）
+        a[k] = aux[i++];
+      } else if (less(aux[j], aux[i])) {
+        // 右半边的当前元素小于左半边的当前元素（取右边的元素）
+        a[k] = aux[j++];
+      } else {
+        // 左半边的当前元素小于右半边的当前元素（取左边的元素）
+        a[k] = aux[i++];
+      }
+    }
+  }
+  void sort(vector<string> &a, int lo, int hi) {
+    if (hi <= lo)
+      return;
+    int mid = lo + (hi - lo) / 2;
+    cout << "mid: " << mid << endl;
+    // 将左半边排序
+    sort(a, lo, mid);
+    // 将右半边排序
+    sort(a, mid + 1, hi);
+    // 归并结果
+    merge(a, lo, mid, hi);
+  }
+};
+
 int main() {
   vector<string> vec;
   string buf;
@@ -117,7 +184,8 @@ int main() {
 
   // Selection s;
   // Insertion s;
-  Shell s;
+  // Shell s;
+  Merge s;
   s.sort(vec);
   s.show(vec);
 
